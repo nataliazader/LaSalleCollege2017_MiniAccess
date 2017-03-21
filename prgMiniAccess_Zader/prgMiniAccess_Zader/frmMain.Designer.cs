96ccd9d2eb31139a6f341127d69bfeb5719e1600
nataliazader <natalia.zader@gmail.com>
2017-03-20 21:48:26 -0400

    Added display info

--- prgMiniAccess_Zader/prgMiniAccess_Zader/frmMain.Designer.cs
+++ prgMiniAccess_Zader/prgMiniAccess_Zader/frmMain.Designer.cs
@@ -28,24 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
             this.databaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
+            this.tableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelDatabase = new System.Windows.Forms.Panel();
+            this.labelDatabase = new System.Windows.Forms.Label();
+            this.listBoxInfo = new System.Windows.Forms.ListBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.menuStripMain.SuspendLayout();
+            this.panelDatabase.SuspendLayout();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // menuStripMain
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.databaseToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.databaseToolStripMenuItem,
+            this.tableToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(784, 24);
+            this.menuStripMain.TabIndex = 0;
+            this.menuStripMain.Text = "menuStrip1";
             // 
             // databaseToolStripMenuItem
             // 
@@ -61,6 +74,7 @@
             // newToolStripMenuItem
             // 
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
             this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.newToolStripMenuItem.Text = "New";
             this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
@@ -68,6 +82,7 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
             this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
@@ -81,21 +96,119 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // tableToolStripMenuItem
+            // 
+            this.tableToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem1,
+            this.openToolStripMenuItem1,
+            this.toolStripMenuItem3,
+            this.exitToolStripMenuItem1});
+            this.tableToolStripMenuItem.Enabled = false;
+            this.tableToolStripMenuItem.Name = "tableToolStripMenuItem";
+            this.tableToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.tableToolStripMenuItem.Text = "Table";
+            // 
+            // newToolStripMenuItem1
+            // 
+            this.newToolStripMenuItem1.Name = "newToolStripMenuItem1";
+            this.newToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem1.Size = new System.Drawing.Size(178, 22);
+            this.newToolStripMenuItem1.Text = "New";
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(178, 22);
+            this.openToolStripMenuItem1.Text = "Open";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(178, 22);
+            this.toolStripMenuItem3.Text = "  ";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(178, 22);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            // 
+            // panelDatabase
+            // 
+            this.panelDatabase.Controls.Add(this.labelName);
+            this.panelDatabase.Controls.Add(this.labelInfo);
+            this.panelDatabase.Controls.Add(this.listBoxInfo);
+            this.panelDatabase.Controls.Add(this.labelDatabase);
+            this.panelDatabase.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDatabase.Location = new System.Drawing.Point(0, 24);
+            this.panelDatabase.Name = "panelDatabase";
+            this.panelDatabase.Size = new System.Drawing.Size(784, 437);
+            this.panelDatabase.TabIndex = 1;
+            this.panelDatabase.Visible = false;
+            // 
+            // labelDatabase
+            // 
+            this.labelDatabase.AutoSize = true;
+            this.labelDatabase.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDatabase.Location = new System.Drawing.Point(310, 15);
+            this.labelDatabase.Name = "labelDatabase";
+            this.labelDatabase.Size = new System.Drawing.Size(105, 13);
+            this.labelDatabase.TabIndex = 0;
+            this.labelDatabase.Text = "Database Name: ";
+            // 
+            // listBoxInfo
+            // 
+            this.listBoxInfo.FormattingEnabled = true;
+            this.listBoxInfo.HorizontalScrollbar = true;
+            this.listBoxInfo.Location = new System.Drawing.Point(125, 60);
+            this.listBoxInfo.Name = "listBoxInfo";
+            this.listBoxInfo.Size = new System.Drawing.Size(630, 290);
+            this.listBoxInfo.TabIndex = 2;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInfo.Location = new System.Drawing.Point(20, 60);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(78, 13);
+            this.labelInfo.TabIndex = 3;
+            this.labelInfo.Text = "Information :";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(421, 15);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(0, 13);
+            this.labelName.TabIndex = 4;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Controls.Add(this.panelDatabase);
+            this.Controls.Add(this.menuStripMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStripMain;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmMain";
             this.Text = "Mini Access";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            this.panelDatabase.ResumeLayout(false);
+            this.panelDatabase.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,12 +216,22 @@
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStripMain;
         private System.Windows.Forms.ToolStripMenuItem databaseToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.Panel panelDatabase;
+        private System.Windows.Forms.Label labelDatabase;
+        private System.Windows.Forms.ListBox listBoxInfo;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Label labelName;
     }
 }
 
--- prgMiniAccess_Zader/prgMiniAccess_Zader/frmMain.cs
+++ prgMiniAccess_Zader/prgMiniAccess_Zader/frmMain.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using DAO;
 
 namespace prgMiniAccess_Zader
 {
     public partial class frmMain : Form
     {
+        DBEngine dbe = new DBEngine();
+        Database db;
         public frmMain()
         {
             InitializeComponent();
@@ -21,12 +24,18 @@ namespace prgMiniAccess_Zader
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Create a new database";
+            saveFileDialog.InitialDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\App_Data";
             saveFileDialog.Filter = "Database files(*.accdb)|*.accdb";
             saveFileDialog.RestoreDirectory = true;
 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                MessageBox.Show(saveFileDialog.FileName);
+                if (File.Exists(saveFileDialog.FileName))
+                    File.Delete(saveFileDialog.FileName);
+                db = dbe.CreateDatabase(saveFileDialog.FileName, LanguageConstants.dbLangGeneral);
+                labelName.Text = Path.GetFileName(saveFileDialog.FileName);
+                displayInfo();
             }
         }
 
@@ -40,7 +49,9 @@ namespace prgMiniAccess_Zader
             openFileDialog.RestoreDirectory = true;
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                MessageBox.Show(openFileDialog.FileName);
+                db = dbe.OpenDatabase(openFileDialog.FileName);
+                labelName.Text = Path.GetFileName(openFileDialog.FileName);
+                displayInfo();
             }
         }
 
@@ -51,6 +62,34 @@ namespace prgMiniAccess_Zader
             if ((int)dialogResult == 6)
             {
                 this.Close();
+            }
+        }
+
+        private void displayInfo() {
+            panelDatabase.Visible = tableToolStripMenuItem.Enabled = true;
+            listBoxInfo.Items.Clear();
+            listBoxInfo.Items.Add("Path : " + db.Name);
+            listBoxInfo.Items.Add("Number of tables : " + db.TableDefs.Count);
+
+            foreach (TableDef oneTable in db.TableDefs)
+            {
+                listBoxInfo.Items.Add("\tTable:" + oneTable.Name);
+                foreach (Field field in oneTable.Fields)
+                    listBoxInfo.Items.Add("\tField:" + field.Name);
+                //foreach (Index inx in oneTable.Indexes)
+                //    listBoxInfo.Items.Add("\tIndex:" + inx.Name);
+            }
+
+            foreach (Relation rel in db.Relations)
+            {
+                listBoxInfo.Items.Add("RElation Name:" + rel.Name);
+                listBoxInfo.Items.Add("RElation Name:" + rel.Table);
+                listBoxInfo.Items.Add("RElation Name:" + rel.ForeignTable);
+                foreach (Field fd in rel.Fields)
+                {
+                    listBoxInfo.Items.Add("field:" + fd.Name);
+                    listBoxInfo.Items.Add("field:" + fd.ForeignName);
+                }
             }
         }
     }
